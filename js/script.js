8231c1ad8fee7e2b1086b494c0a481769167c0b4
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-10 10:41:44 -0400

    add budget links

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
+// Ativar Link
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-  if (url.includes(href)){
-    link.classList.add("ativo")
+  if (url.includes(href)) {
+    link.classList.add("ativo");
   }
 }
-links.forEach(ativarLink)
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  };
+};
+parametros.forEach(ativarProduto);

3bffa1a3c9bf9d88c6959964bc04c7bad4033bd7
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-16 10:42:01 -0400

    add form javascript and functions

--- js/formulario.js
+++ js/formulario.js
@@ -1,7 +1,26 @@
 const formulario = document.querySelector("form");
 
-function enviarFormulario(event) {
-
+function formularioEnviado(resposta) {
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p>Mensagem enviada. Em breve entraremos em contato.</p>";
+  } else {
+    formulario.innerHTML =
+      "<p>Erro. Mande no nosso email contato@email.com</p>";
+  }
 }
 
-formulario.addEventListener("submit",enviarFormulario);
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disabled = "true";
+  botao.innerText = "Enviando...";
+
+  const data = FormData(formulario);
+  fetch("../enviar.php", {
+    method: "POST",
+    body: data,
+  }).then(formularioEnviado);
+}
+
+formulario.addEventListener("submit", enviarFormulario);
